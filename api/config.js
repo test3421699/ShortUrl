a03b9ac444948c5764b5337de991341571228bbd
test3421699 <test91674070@gmail.com>
2026-07-12 11:01:50 +0530

    update

--- api/config.js
+++ api/config.js
@@ -1,8 +1,12 @@
 export default function handler(req, res) {
+  res.setHeader('Content-Type', 'application/json');
   res.status(200).json({
-    apiKey: process.env.FIREBASE_CLIENT_API_KEY,
-    authDomain: process.env.FIREBASE_CLIENT_AUTH_DOMAIN,
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
     projectId: process.env.FIREBASE_PROJECT_ID,
-    databaseURL: process.env.FIREBASE_DATABASE_URL
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
   });
 }
